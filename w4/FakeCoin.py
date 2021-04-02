01d7e575f6592367873a8c30765ff8a29b657e34
dudt <dang.du259@gmail.com>
2021-04-02 20:39:33 +0700

    2/4/2021: dudt commit w4

--- w4/FakeCoin.py
+++ w4/FakeCoin.py
@@ -1,16 +1,11 @@
-coin = [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1]
-# coin_real > coin_fake
 import random
-import enum
 
-Scale = enum.Enum(
-    "Scale", ["LeftHeavy", "RightHeavy", "Balanced"]
-)
-
+# coin_real > coin_fake
 coin_real = 2
 coin_fake = 1
 
 
+# tạo ngẫu nhiên listcoint có size = n
 def random_coins(n):
     """A shuffled list of (n-1) real coins and 1 fake coin."""
     assert n > 0
@@ -19,6 +14,7 @@ def random_coins(n):
     return coins
 
 
+# tính tổng cân nặng coin trong list từ s đến e
 def sumCoin(listcoin, s, e):  # sum[s,e]
     sum = 0
     for i in range(s, e + 1):
@@ -26,71 +22,50 @@ def sumCoin(listcoin, s, e):  # sum[s,e]
     return sum
 
 
-# def find_fake_coin(listcoin, s, e, pivot):  # listcoin, 0, 9
-# n = e - s + 1
-# # Chuỗi lẻ
-# if (n % 2 != 0):
-#     # điểm ở giữa
-#     mid = int((e - s) // 2) + chot
-#     sum_left = sumCoin(listcoin, 0, mid-1)
-#     sum_right = sumCoin(listcoin, mid+1, n-1)
-#     if (sum_left == sum_right):
-#         return mid
-#     elif (sum_left > sum_right):  # right => (mid+1 , e)
-#         chot = chot + mid
-#         return find_fake_coin(listcoin, mid + 1, e, chot)
-#     else:  # left => (s, mid-1)
-#         chot = chot - mid
-#         return find_fake_coin(listcoin, s, mid - 1, chot)
-# # chuỗi chẵn
-# else:
-#     # điểm ở giữa
-#     # mid = none
-#     note = (e - s) // 2  # note = 4
-#     sum_left = sumCoin(listcoin, 0, note)  # left = (0, note)
-#     sum_right = sumCoin(listcoin, note + 1, e)  # right = (note+1, n)
-#     print(sum_left)
-#     print(sum_right)
-#     if (sum_left == sum_right):
-#         return None
-#     elif (sum_left > sum_right):
-#         return find_fake_coin(listcoin, note + 1, e)  # left > right => right =>  (note+1, e)
-#     else:
-#         return find_fake_coin(listcoin, s, note)  # left < right => left => (0, note)
-def find_fake_coin(list_coin, s, e, pivot):
+# tìm đồng xu giả
+def find_fake_coin(list_coin, s, e):
     n = (e - s) + 1
     if (n % 2 == 0):
-        mid = n // 2 - 1
+        mid = n // 2 - 1 + s
         sum_left = sumCoin(list_coin, s, mid)
         sum_right = sumCoin(list_coin, mid + 1, e)
         if (sum_left == sum_right):
             return None
         elif (sum_left < sum_right):
-            if(pivot > mid):
-                pivot = pivot - mid
-            else:
-                pivot = pivot + mid
-            return find_fake_coin(list_coin, s, mid, pivot)
+            return find_fake_coin(list_coin, s, mid)
         else:
-            pivot = pivot + mid
-            return find_fake_coin(list_coin, mid + 1, e, pivot)
+            return find_fake_coin(list_coin, mid + 1, e)
     else:
-        mid = n // 2
-        sum_left = sumCoin(list_coin, s, mid)
+        mid = n // 2 + s
+        sum_left = sumCoin(list_coin, s, mid - 1)
         sum_right = sumCoin(list_coin, mid + 1, e)
         if (sum_left == sum_right):
             return mid
         elif (sum_left < sum_right):
-            pivot = pivot - mid
-            return find_fake_coin(list_coin, s, mid, pivot)
+            return find_fake_coin(list_coin, s, mid - 1)
         else:
-            pivot = pivot + mid
-            return find_fake_coin(list_coin, mid + 1, e, pivot)
+            return find_fake_coin(list_coin, mid + 1, e)
 
-
-
-# list_coin = random_coins(10)
-list_coin = [2, 2, 2, 1, 2, 2, 2, 2, 2, 2]
-print(len(list_coin))
+#kiểm tra 1 lần với danh sách đồng xu cố định
+print("* kiểm tra 1 lần với chuỗi cố định ")
+list_coin = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
 print(list_coin)
-print(find_fake_coin(list_coin=list_coin, s=0, e=(len(list_coin) - 1), pivot=0))
+print("Fake Coin Position: "+str(find_fake_coin(list_coin=list_coin, s=0, e=(len(list_coin) - 1))))
+print("-----------------------")
+
+#kiểm tra 1 lần với danh sách đồng xu ngẫu nhiên
+print("* kiểm tra 1 lần với danh sách đồng xu ngẫu nhiên ")
+list_coin = random_coins(10)
+print(list_coin)
+print("Fake Coin Position: "+str(find_fake_coin(list_coin=list_coin, s=0, e=(len(list_coin) - 1))))
+print("-----------------------")
+
+#kiểm tra n lần với danh sách đồng xu ngẫu nhiên
+print("* kiểm tra n lần với danh sách đồng xu ngẫu nhiên")
+print("Nhập vào số lần bạn muốn kiểm tra: ")
+n = int(input())
+for i in range(0, n):
+    list_coin = random_coins(10)
+    print(list_coin)
+    print("Fake Coin Position: "+str(find_fake_coin(list_coin=list_coin, s=0, e=(len(list_coin) - 1))))
+    print("-----------------------")
