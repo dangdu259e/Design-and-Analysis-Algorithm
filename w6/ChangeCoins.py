fe943c78bc41514ea9cb78d250e192a0e891aa72
dudt <dang.du259@gmail.com>
2021-04-23 06:38:38 +0700

    23/04/2021: dudt commit w5

--- w6/ChangeCoins.py
+++ w6/ChangeCoins.py
@@ -1,8 +1,12 @@
 # change coins using greedy
+import random
+
+import numpy as np
+
 
 # A is denomination, T amount money
 def change_coins(A, T):
-    n = len(A)
+    n = len(A)  # size of list denomination
     A.sort()
     print('list coins = ' + str(A))
     S = []  # solution: S[i] number of coins A[i]
@@ -20,17 +24,29 @@ def change_coins(A, T):
     else:
         return S
 
-def create_random_list_coins (n):  # n = size list coins
-    for i in range(0, n):
-        i = i + 1
 
-    return 0
+def create_random_list_coins(n):  # n = size list coins
+    A = list(range(1, n))
+    random.shuffle(A)
+    return A
+
 
 # hand create input list and amount money
+print("handle create input list and amount money")
 A = [10, 2, 3, 4, 5, 6]
 print("input list coin = " + str(A))
 T = 50
 print("T amount money = " + str(T))
 print("solution: " + str(change_coins(A, T)))
+
+print("-------------------------------")
+
+# random input
+print("random input")
+a = create_random_list_coins(10)
+print("input list coin = " + str(a))
+t = np.random.randint(1, 100, dtype=int)  # create random amount money
+print("T amount money = " + str(t))
+print("solution: " + str(change_coins(a, t)))
 
 # auto create input list and amount money
